c69ba8b65f4f54291f1dd0aa8546ae7c2ea3c155
Kerneltyu <32785087+takato86@users.noreply.github.com>
2020-03-25 21:02:25 +0900

    [bugfix] staticfiles_dirs.

--- subgoal_transfer_experiment/settings.py
+++ subgoal_transfer_experiment/settings.py
@@ -114,9 +114,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-)
+]
+
 
 
 # Database
